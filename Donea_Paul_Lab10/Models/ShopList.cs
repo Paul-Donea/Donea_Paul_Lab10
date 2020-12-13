ac6ea4a64060e5ebcecf493809e61c1c774649d7
PAUL DONEA <paul.donea@stud.ubbcluj.ro>
2020-12-13 22:58:35 +0200

    Add project files.

---
+++ Donea_Paul_Lab10/Models/ShopList.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Donea_Paul_Lab10.Models
+{
+     public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
